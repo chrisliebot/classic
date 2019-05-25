abca535fbfaf1ad5b9b1ee4c152615bced109b43
Chrisliebär <chrisliebot@kd-tree.com>
2019-05-25 15:23:04 +0200

    Implement unicode control sequences for unicode command

--- src/main/java/chrisliebaer/chrisliebot/command/unicode/UnicodeCommand.java
+++ src/main/java/chrisliebaer/chrisliebot/command/unicode/UnicodeCommand.java
@@ -6,10 +6,15 @@ import chrisliebaer.chrisliebot.command.CommandExecutor;
 import org.apache.commons.codec.binary.Hex;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class UnicodeCommand implements CommandExecutor {
+	
+	private static final int MAX_CODEPOINT_DISPLAY = 5;
 	
 	private static final Pattern CODEPOINT_INPUT = Pattern.compile("^U\\+(?<cp>[0-9A-Fa-f]{1,8})$");
 	
@@ -17,36 +22,37 @@ public class UnicodeCommand implements CommandExecutor {
 	public void execute(Message m, String arg) {
 		arg = arg.trim();
 		
-		int cp;
+		List<Integer> cps;
 		var matcher = CODEPOINT_INPUT.matcher(arg);
-		if (matcher.matches()) {
+		if (matcher.matches()) { // matches on input that is describing raw code points without encoding
 			try {
-				cp = Integer.parseInt(matcher.group("cp"), 16);
+				cps = new ArrayList<>(1);
+				cps.add((int) Long.parseLong(matcher.group("cp"), 16));
 			} catch (NumberFormatException e) {
 				m.reply(C.error("Das sah zwar gut aus, aber ich habs trotzdem nicht gerafft."));
 				return;
 			}
-		} else {
-			cp = arg.codePointAt(0);
+		} else { // parse input as is
+			cps = arg.codePoints().boxed().collect(Collectors.toList());
 		}
 		
-		
-		if (!Character.isValidCodePoint(cp)) {
-			m.reply(C.error("Das ist kein gültiger Codepoint."));
-			return;
-		}
-		
+		String input = cps.stream().map(cp -> String.valueOf(Character.toChars(cp))).collect(Collectors.joining(", "));
+		m.reply("Eingabe war: " + C.highlight(input) + ", zeige ersten " + MAX_CODEPOINT_DISPLAY + " von " + cps.size() + " Codepoints.");
+		cps.stream().limit(MAX_CODEPOINT_DISPLAY).forEachOrdered(i -> printCodePoint(i, m));
+	}
+	
+	private void printCodePoint(int cp, Message m) {
 		Optional<String> name = Optional.ofNullable(Character.getName(cp));
 		var cpStr = Integer.toHexString(cp).toUpperCase();
 		var str = Character.toString(cp);
 		var utf8 = Hex.encodeHexString(str.getBytes(StandardCharsets.UTF_8)).toUpperCase();
 		var utf16 = Hex.encodeHexString(str.getBytes(StandardCharsets.UTF_16LE)).toUpperCase();
 		
-		m.reply(String.format("Eingabe: %s, Codepoint: U+%s, UTF-8: 0x%s, UTF-16LE: 0x%s%s",
-				str,
-				cpStr,
-				utf8,
-				utf16,
-				name.map(s -> ", Name: " + s).orElse(", UNBEKANNT")));
+		m.reply(String.format("Display: %s, Codepoint: %s, UTF-8: %s, UTF-16LE: %s%s",
+				C.highlight(str),
+				C.highlight("U+" + cpStr),
+				C.highlight("0x" + utf8),
+				C.highlight("0x" + utf16),
+				name.map(s -> ", Name: " + C.highlight(s)).orElse(", UNBEKANNT")));
 	}
 }
