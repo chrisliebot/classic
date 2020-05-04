50c9e8dd76956bfe283df0a3dfe7186d05531563
Chrisliebär <chrisliebot@kd-tree.com>
2020-05-04 22:51:19 +0200

    Add null command for disabling certain commands in specific situations

---
+++ src/main/java/chrisliebaer/chrisliebot/command/basic/NullCommand.java
@@ -0,0 +1,12 @@
+package chrisliebaer.chrisliebot.command.basic;
+
+import chrisliebaer.chrisliebot.command.ChrislieListener;
+
+/**
+ * This listener can be used to override existing listeners, since it doesn't provide any logic and will just silently eat the invocation.
+ */
+public class NullCommand implements ChrislieListener.Command {
+	
+	@Override
+	public void execute(Invocation invc) throws ListenerException {}
+}
