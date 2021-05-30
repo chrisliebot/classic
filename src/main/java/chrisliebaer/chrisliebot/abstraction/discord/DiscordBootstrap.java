47d75bffd46ce3bb0538268356e136cf09a89ed8
Chrisliebär <mail@chrisliebaer.de>
2021-05-30 11:35:51 +0200

    Make Chrisliebot more human

--- src/main/java/chrisliebaer/chrisliebot/abstraction/discord/DiscordBootstrap.java
+++ src/main/java/chrisliebaer/chrisliebot/abstraction/discord/DiscordBootstrap.java
@@ -3,6 +3,7 @@ package chrisliebaer.chrisliebot.abstraction.discord;
 import chrisliebaer.chrisliebot.Chrisliebot;
 import chrisliebaer.chrisliebot.abstraction.ServiceBootstrap;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.hooks.AnnotatedEventManager;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
@@ -16,6 +17,7 @@ public class DiscordBootstrap implements ServiceBootstrap {
 	public DiscordService service(Chrisliebot bot, String identifier) throws LoginException {
 		var jda = JDABuilder.create(token, GatewayIntent.getIntents(GatewayIntent.ALL_INTENTS))
 				.setEventManager(new AnnotatedEventManager())
+				.setActivity(Activity.playing("mit dir"))
 				.build();
 		return new DiscordService(bot, jda, identifier);
 	}
