8afa8932941033e0f07b85c158d8823c5f85f0c3
Chrisliebär <mail@chrisliebaer.de>
2021-11-30 02:32:56 +0100

    Add proxy support for debugging web requests

--- src/main/java/chrisliebaer/chrisliebot/SharedResources.java
+++ src/main/java/chrisliebaer/chrisliebot/SharedResources.java
@@ -11,12 +11,17 @@ import okhttp3.logging.HttpLoggingInterceptor;
 import org.mariadb.jdbc.MariaDbPoolDataSource;
 
 import javax.sql.DataSource;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 import java.sql.SQLException;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 
 @Slf4j
 public class SharedResources extends AbstractIdleService {
+	
+	private static final String HTTP_PROXY_HOST = "HTTP_PROXY_HOST";
+	private static final String HTTP_PROXY_PORT = "HTTP_PROXY_PORT";
 	
 	private static final String DEFAULT_USER_AGENT = "Chrisliebot/" + VersionUtil.version() + " (+https://github.com/chrisliebot)";
 	
@@ -49,10 +54,26 @@ public class SharedResources extends AbstractIdleService {
 			log.trace("HTTP REQUEST: {}", s);
 		});
 		httpLogger.setLevel(HttpLoggingInterceptor.Level.BASIC);
-		httpClient = new OkHttpClient.Builder()
+		
+		
+		var builder = new OkHttpClient.Builder()
 				.addNetworkInterceptor(c -> c.proceed(c.request().newBuilder().header("User-Agent", DEFAULT_USER_AGENT).build()))
-				.addNetworkInterceptor(httpLogger)
-				.build();
+				.addNetworkInterceptor(httpLogger);
+		
+		// check for proxy in properties for debugging
+		if (System.getProperty(HTTP_PROXY_HOST) != null || System.getProperty(HTTP_PROXY_PORT) != null) {
+			try {
+				var proxyHost = System.getProperty(HTTP_PROXY_HOST, "localhost");
+				var proxyPort = System.getProperty(HTTP_PROXY_PORT, "8080");
+				log.info("Using proxy via {}:{}", proxyHost, proxyPort);
+				Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyHost, Integer.parseInt(proxyPort)));
+				builder.proxy(proxy);
+			} catch (NumberFormatException e) {
+				log.error("invalid port for proxy", e);
+			}
+		}
+		httpClient = builder.build();
+		
 		timer = new ScheduledThreadPoolExecutor(1, r -> {
 			var t = new Thread(r, "SharedTimerExecutor");
 			t.setDaemon(true);
